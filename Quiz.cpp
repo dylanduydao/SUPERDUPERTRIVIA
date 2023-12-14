b30c9b015a953dfda37865c32769701c668fe408
dylanduydao <114511780+dylanduydao@users.noreply.github.com>
2023-12-14 13:36:38 -0500

    Add files via upload

--- Quiz.cpp
+++ Quiz.cpp
@@ -10,101 +10,107 @@ Quiz::Quiz() : score(0) {}
 
 void Quiz::loadQuestions() {
   questions.push_back(std::make_shared<Question>(
-      "Who painted the Mona Lisa?", "Sigmond Freid", "Mother Teresa",
-      "Leonardo Da Vinci", "Vittal Harrith",
+      "Who painted the Mona Lisa?", " Sigmond Freid", " Mother Teresa",
+      " Leonardo Da Vinci", " Craig",
       'c' // Correct answer as a character
-  ));
+      ));
 
   questions.push_back(std::make_shared<Question>(
       "Elon Musk, the new owner of Twitter, said that he will revive this "
       "social media platform. What platform was it?",
-      "MySpace", "Vine", "Club Penguin", "OnlyFriends",
+      " MySpace", " Vine", " Club Penguin", " OnlyFriends",
       'b' // Correct answer as a character
-  ));
+      ));
 
-  questions.push_back(std::make_shared<Question>(
-      "What does a Camel store in its hump?",
-      "Water", "Fat", "Blood", "Urine",
-      'b' // Correct answer as a character
-  ));
+  questions.push_back(
+      std::make_shared<Question>("What does a Camel store in its hump?",
+                                 " Water", " Fat", " Blood", " Urine",
+                                 'b' // Correct answer as a character
+                                 ));
 
   questions.push_back(std::make_shared<Question>(
       "What are all the layers inside Earth?",
-      "Crust, Mantle, Outer Core, Inner Core",
-      "Surface, Deeper Core, Inner Core, Center Core",
-      "Crust, Inner Core, Outer Center, Nucleus", "Fee, Fye, Foe, Fum",
+      " Crust, Mantle, Outer Core, Inner Core",
+      " Surface, Deeper Core, Inner Core, Center Core",
+      " Crust, Inner Core, Outer Center, Nucleus", " Fee, Fye, Foe, Fum",
       'a' // Correct answer as a character
-  ));
+      ));
 
   questions.push_back(std::make_shared<Question>(
-      "What sport could you receive the Heisman Trophy?", "Basketball",
-      "American Football", "Cricket", "Golf",
+      "What sport could you receive the Heisman Trophy?", " Basketball",
+      " American Football", " Cricket", " Golf",
       'b' // Correct answer as a character
-  ));
+      ));
 
   questions.push_back(std::make_shared<Question>(
       "In the cartoon “Spongebob Squarepants” which holiday was Spongebob "
       "excited for, that made him go “Hinga Dinga Durgen!”?",
-      "Inauguration Day", "Scrabble Day", "Leif Erikson Day",
-      "Arbor Day",
+      " Inauguration Day", " Scrabble Day", " Leif Erikson Day", " Arbor Day",
       'c' // Correct answer as a character
-  ));
+      ));
 
   // Add more questions here...
 }
 
 void Quiz::start() {
-  std::cout << "WELCOME TO SUPER DUPER TRIVIA!\n ";
-  std::cout << "What is your team name? ";
-  std::cin >> teamName;
-  std::cout << "Let's give it up for, " << teamName << "!" << std::endl;
+  try {
+    std::cout << "Welcome to SUPER DUPER TRIVIA!\n What is your team name? ";
+    std::cin >> teamName;
 
-  for (const auto &question : questions) {
-    displayQuestion(question);
+    if (teamName.empty()) {
+      throw std::invalid_argument("Team name cannot be empty.");
+    }
+
+    std::cout << "Let's give it up for, " << teamName << "!" << std::endl;
+
+    for (const auto &question : questions) {
+      displayQuestion(question);
+
+      char userAnswer;
+      bool validInput = false;
+
+      while (!validInput) {
+        std::cout << "Your answer (A/B/C/D): ";
+        std::cin >> userAnswer;
+        userAnswer = std::toupper(userAnswer);
+
+        if (userAnswer == 'A' || userAnswer == 'B' || userAnswer == 'C' ||
+            userAnswer == 'D') {
+          validInput = true;
+        } else {
+          throw std::invalid_argument(
+              "Invalid input. Please enter 'A', 'B', 'C', or 'D'.");
+        }
+      }
+
+      if (question->isCorrect(userAnswer)) {
+        std::cout << "BOOM! Correct!" << std::endl;
+        score++;
+      } else {
+        std::cout << "Incorrect. The correct answer was: "
+                  << question->getCorrectAnswer() << std::endl;
+      }
+
+      std::cout << std::endl;
+    }
+
+    std::cout << teamName << ", your score: " << score << "/"
+              << questions.size() << std::endl;
+  } catch (const std::exception &ex) {
+    std::cerr << "An error occurred: " << ex.what() << std::endl;
   }
-
-  std::cout << teamName << ", you got a: " << score << "/" << questions.size() << std::endl;
 }
 
 void Quiz::displayQuestion(const std::shared_ptr<Question> &question) {
-  std::cout << "+----------------------------------------+" << std::endl;
-  std::cout << "| Question: " << std::endl;
-  std::cout << "| " << question->getQuestionText() << std::endl;
-  std::cout << "+----------------------------------------+" << std::endl;
-
-  std::cout << "| Choices: " << std::endl;
+  // Display the question with boxes
+  std::cout << "-----------------------------" << std::endl;
+  std::cout << "| Question: " << question->getQuestionText() << std::endl;
+  std::cout << "| Options:" << std::endl;
   std::cout << "| A. " << question->getOptionA() << std::endl;
   std::cout << "| B. " << question->getOptionB() << std::endl;
   std::cout << "| C. " << question->getOptionC() << std::endl;
   std::cout << "| D. " << question->getOptionD() << std::endl;
-  std::cout << "+----------------------------------------+" << std::endl;
-
-  char userAnswer;
-  bool validInput = false;
-
-  while (!validInput) {
-    std::cout << "Your answer (A/B/C/D): ";
-    std::cin >> userAnswer;
-    userAnswer = std::toupper(userAnswer);
-
-    if (userAnswer == 'A' || userAnswer == 'B' || userAnswer == 'C' || userAnswer == 'D') {
-      validInput = true;
-    } else {
-      std::cerr << "+----------------------------------------+" << std::endl;
-      std::cerr << "| Error: Please enter 'A', 'B', 'C', or 'D'." << std::endl;
-      std::cerr << "+----------------------------------------+" << std::endl;
-    }
-  }
-
-  std::cout << "+----------------------------------------+" << std::endl;
-  if (question->isCorrect(userAnswer)) {
-    std::cout << "| BOOM! Correct!" << std::endl;
-    score++;
-  } else {
-    std::cout << "| OOF! Incorrect... The correct answer was: " << question->getCorrectAnswer() << std::endl;
-  }
-  std::cout << "+----------------------------------------+" << std::endl;
-  std::cout << std::endl;
+  std::cout << "-----------------------------" << std::endl;
 }
 
 // Destructor definition
